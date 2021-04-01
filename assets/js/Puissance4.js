c6d5279da1b9fa3f0d62dd926c650472ef4c57d5
Anthony5997 <anthony.rita@outlook.com>
2021-04-01 13:56:11 +0200

    Ajout condition de victoire Horizontal -  Verfi gauche et droite / Alerte en cas de victoire et debbug de la condition de victoire Verticale

--- assets/js/Puissance4.js
+++ assets/js/Puissance4.js
@@ -63,9 +63,11 @@ class Puissance4{
   }
 
   checkWin(divJouer){
-
-
+    let count = 1
     this.checkVertical(divJouer)
+    count = this.checkHorizontalGauche(divJouer, count)
+    console.log(count);
+    this.checkHorizontalDroite(divJouer,count)
   }
 
   checkVertical(divJouer){
@@ -73,112 +75,79 @@ class Puissance4{
     let idDivJouer = divJouer.getAttribute('data-row')-1
     let col = divJouer.parentNode.children
 
+
     while(col[idDivJouer +1] !== undefined){
       if(col[idDivJouer +1].classList.contains(this.joueurTurn.getColor()) == divJouer.classList.contains(this.joueurTurn.getColor())){
-        console.log("good +1")
         count++
         divJouer = col[idDivJouer +1]
         idDivJouer = divJouer.getAttribute('data-row')-1
         if (count === 4) {
-          return console.log('WINNERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
+          return alert("Player " + this.joueurTurn.getColor() + " Win!")
         }
       }else{
-        console.log('pas de la meme couleur en dessous, reset du compteur')
-        count = 1
         break
       }
     }
   }
 
-  checkHorizontal(divJouer){
-    let count = 1
-    let idDivJouer = divJouer.getAttribute('data-row')-1
-    let col = divJouer.parentNode.children
-
-    while(col[idDivJouer +1] !== undefined){
-      if(col[idDivJouer +1].classList.contains(this.joueurTurn.getColor()) == divJouer.classList.contains(this.joueurTurn.getColor())){
-        console.log("good +1")
-        count++
-        divJouer = col[idDivJouer +1]
-        idDivJouer = divJouer.getAttribute('data-row')-1
+  checkHorizontalDroite(divJouer, count){
+    
+    let idRowJouer = parseInt(divJouer.getAttribute('data-row')) // row de la col jouer
+    let arrayRow = divJouer.parentNode.children // tout les row de notre col jouer
+    let arrayCol = divJouer.parentNode.parentNode.children // toute les colones 
+    let idColJouer = parseInt(divJouer.parentNode.getAttribute('data-col')) // id de la col jouer
+    let countColAfter = 1
+    
+   // console.log("LOG DE IDROWJOUER -1", idRowJouer);
+    
+    
+   
+   console.log("DivJouer = ",arrayCol[ parseInt(idColJouer) + countColAfter])
+ 
+    while(arrayCol[idColJouer -countColAfter] !== undefined &&
+          arrayCol[idColJouer -countColAfter].children[idRowJouer -1].classList.contains(this.joueurTurn.getColor()) == arrayCol[idColJouer].children[idRowJouer -1].classList.contains(this.joueurTurn.getColor())){
+     
+            console.log(countColAfter);
+            console.log("!Valeur array Id coljouer -1",arrayCol[idColJouer -countColAfter].children[idRowJouer -1]);
+            console.log("Valeur array Id col cliquer",arrayCol[idColJouer]);
+            countColAfter++
+            count++
+            console.log("Compteur = ",count);
         if (count === 4) {
-          return console.log('WINNERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
+          return alert("Player " + this.joueurTurn.getColor() + " Win!")
         }
-      }else{
-        console.log('pas de la meme couleur en dessous, reset du compteur')
-        count = 1
-        break
-      }
     }
   }
-    /*
+  checkHorizontalGauche(divJouer, count ){
+  
+    let idRowJouer = parseInt(divJouer.getAttribute('data-row')) // row de la col jouer
+    let arrayRow = divJouer.parentNode.children // tout les row de notre col jouer
+    let arrayCol = divJouer.parentNode.parentNode.children // toute les colones 
+    let idColJouer = parseInt(divJouer.parentNode.getAttribute('data-col')) // id de la col jouer
+    let countColBefore = 1
+    
+   // console.log("LOG DE IDROWJOUER -1", idRowJouer);
     
     
-    if (col[idDivJouer +1] !== undefined ) {
-      console.log(col[idDivJouer +1])
-      if (col[idDivJouer +1].classList.contains(this.joueurTurn.getColor()) == divJouer.classList.contains(this.joueurTurn.getColor()) ) {
-          console.log("good +1")
-          count++
-          divJouer = col[idDivJouer +1]
-          idDivJouer = divJouer.getAttribute('data-row')-1
-
-          if (col[idDivJouer +1] !== undefined ) {
-            console.log(col[idDivJouer +1])
-            if (col[idDivJouer +1].classList.contains(this.joueurTurn.getColor()) == divJouer.classList.contains(this.joueurTurn.getColor()) ) {
-                console.log("good +1")
-                count++
-                divJouer = col[idDivJouer +1]
-                idDivJouer = divJouer.getAttribute('data-row')-1
-      
-                if (col[idDivJouer +1] !== undefined ) {
-                  console.log(col[idDivJouer +1])
-                  if (col[idDivJouer +1].classList.contains(this.joueurTurn.getColor()) == divJouer.classList.contains(this.joueurTurn.getColor()) ) {
-                      console.log("good +1")
-                      count++
-                     if (count == 4) {
-                       console.log('WINNERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
-                     }
-            
-                     
-            
-                  }else{
-                    console.log('pas de la meme couleur en dessous')
-                    count = 1
-                  }
-                  
-                }
-      
-            }else{
-              console.log('pas de la meme couleur en dessous')
-              count = 1
-
-            }
-            
-          }
-
-      }else{
-        console.log('pas de la meme couleur en dessous')
-        count = 1
-
-      }
-      
+   
+   console.log("DivJouer = ",arrayCol[ parseInt(idColJouer) + countColBefore])
+ 
+    while(arrayCol[idColJouer +countColBefore] !== undefined &&
+          arrayCol[idColJouer +countColBefore].children[idRowJouer -1].classList.contains(this.joueurTurn.getColor()) == arrayCol[idColJouer].children[idRowJouer -1].classList.contains(this.joueurTurn.getColor())){
+     
+            console.log(countColBefore);
+            console.log("!Valeur array Id coljouer -1",arrayCol[idColJouer +countColBefore].children[idRowJouer -1]);
+            console.log("Valeur array Id col cliquer",arrayCol[idColJouer]);
+            countColBefore++
+            count++
+            console.log("Compteur = ",count);
+        if (count === 4) {
+        
+          return alert("Player " + this.joueurTurn.getColor() + " Win!")
+        }
     }
-    console.log(col)
-    // let count = 0;
-    //   for(let i=0; i< this.cols; i++){
+    return count
+  }
 
-    //     for(let j=0; j<this.rows; j++){
-
-    //       console.log("ROW HERE",this.row);
-
-    //       if(j== this.joueurTurn.getColor()){
-
-    //         console.log("COLOR HERE",this.joueurTurn.getColor())
-    //         count++;
-    //       }
-    //     }
-    //     console.log("COL HERE",this.cols);
-    //   }
-
-    */
+  
 }
